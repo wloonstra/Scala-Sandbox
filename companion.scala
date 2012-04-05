d620109f9e589094e95a344783141bdbf63f8b84
Wiggert <wloonstra@ebay.com>
2012-04-05 08:34:00 +0200

    added companion object example

---
+++ companion.scala
@@ -0,0 +1,26 @@
+class ChecksumAccumulator {
+  private var sum = 0
+  
+  def add(b: Byte) { sum += b } 
+  def checksum(): Int = ~(sum & 0xFF) + 1
+}
+
+import scala.collection.mutable.Map
+
+object ChecksumAccumulator {
+  private val cache = Map[String, Int]()
+  
+  def calculate(s: String): Int =
+    if (cache.contains(s))
+      cache(s)
+    else {
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+}
+
+println(ChecksumAccumulator.calculate("Every value is an object."))

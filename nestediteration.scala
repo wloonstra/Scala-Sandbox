abb274361ca090a858ffd766c8c0dc5f1513b7d0
Wiggert <wloonstra@ebay.com>
2012-04-13 08:09:58 +0200

    forwithcondition and nestediteraion added

---
+++ nestediteration.scala
@@ -0,0 +1,14 @@
+val filesHere = (new java.io.File(".")).listFiles
+
+def fileLines(file: java.io.File) =
+  scala.io.Source.fromFile(file).getLines().toList
+  
+def grep(pattern: String) =
+  for (
+    file <- filesHere
+    if file.getName.endsWith(".scala");
+    line <- fileLines(file)
+    if line.trim.matches(pattern)
+  ) println(file +": " + line.trim)
+  
+grep(".*gcd.*")

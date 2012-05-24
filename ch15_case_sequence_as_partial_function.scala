57895da182005f42191798ca1c1e61d3f5eabe92
Wiggert <wloonstra@ebay.com>
2012-05-24 08:39:40 +0200

    added comments to case sequence as partial function, got explanation from bartosz

--- ch15_case_sequence_as_partial_function.scala
+++ ch15_case_sequence_as_partial_function.scala
@@ -15,6 +15,17 @@ val withDefault2 = (option: Option[Int]) => {
 // is withDefault shorter form of withDefault?
 // or is it technically something else??
 
+// it is just a shortcut.
+// for withDefault2 you could write:
+// val withDefault2: Option[Int] => Int = (option: Option[Int]) => {
+//   blabla
+//}
+// then you do have the type in the value definition. Since the method itself
+// defines more clearly that option is of type Option[Int] the complete type definition
+// after withDefault2: can be omitted. The compiler understand.
+// In case of withDefault: it doens't know what type of Some(x) is, some it's needed tot tell
+// val withDefault: Option[Int] => Int
+
 println(withDefault(Some(10)))
 println(withDefault(None))
 println(withDefault2(Some(10)))

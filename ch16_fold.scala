267a8e6eff16e1dc1da9735fe30aa815ceff1ee2
Wiggert <wloonstra@ebay.com>
2012-06-06 09:00:22 +0200

    foldLeft and foldRight

--- ch16_fold.scala
+++ ch16_fold.scala
@@ -5,3 +5,20 @@
 // op          binary operation
 
 // it's fold LEFT, so starting from a.
+
+// fold right
+// (List(a, b, c) :\ z) (op)   equals   op(a, op(b, op(c, z)))
+
+val words = List("one", "two", "three")
+
+println("one string fold left: " + ( ("" /: words) (_ + " " + _)  ))
+println("one string fold left: " + ( (words.head /: words.tail) (_ + " " + _)  ))
+
+println("one string fold right: " + ( (words :\ "") (_ + " " + _)  ))
+println("one string fold left: " + ( (words.init :\ words.last) (_ + " " + _)  ))
+
+println("character size fold left: " + (  (0 /: words) ((result: Int, element: String) => result + element.length)  ))
+println("character size fold left: " + (  (0 /: words) (_ + _.length)  ))
+println("character size fold right: " + (  (words :\ 0) (_.length + _)  ))
+
+println("one string fold left: " + (  words.tail.foldLeft(words.head) ((result: String, element: String) => result + " " + element))  )

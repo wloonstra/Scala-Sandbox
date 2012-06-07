7e7278cc0ba4aad4327018db8949d420e25cb929
Wiggert <wloonstra@ebay.com>
2012-06-07 08:31:24 +0200

    list fold and methods from object List

--- ch16_fold.scala
+++ ch16_fold.scala
@@ -12,13 +12,30 @@
 val words = List("one", "two", "three")
 
 println("one string fold left: " + ( ("" /: words) (_ + " " + _)  ))
-println("one string fold left: " + ( (words.head /: words.tail) (_ + " " + _)  ))
-
 println("one string fold right: " + ( (words :\ "") (_ + " " + _)  ))
-println("one string fold left: " + ( (words.init :\ words.last) (_ + " " + _)  ))
+
+println("one string fold left: " + ( (words.head /: words.tail) (_ + " " + _)  ))
+println("one string fold right: " + ( (words.init :\ words.last) (_ + " " + _)  ))
 
 println("character size fold left: " + (  (0 /: words) ((result: Int, element: String) => result + element.length)  ))
 println("character size fold left: " + (  (0 /: words) (_ + _.length)  ))
+println("character size fold right: " + (  (words :\ 0) ((element: String, result: Int) => element.length + result)  ))
 println("character size fold right: " + (  (words :\ 0) (_.length + _)  ))
 
-println("one string fold left: " + (  words.tail.foldLeft(words.head) ((result: String, element: String) => result + " " + element))  )
+println("one string fold left: " + (  words.tail.foldLeft(words.head) ((result: String, element: String) => result + " " + element)  ))
+println("one string fold left: " + (  words.tail.foldLeft(words.head) (_ + " " + _)  ))
+println("one string fold right: " + (  words.init.foldRight(words.last) ((result: String, element: String) => result + " " + element)  ))
+println("one string fold right: " + (  words.init.foldRight(words.last) (_ + " " + _)  ))
+
+println("character size fold left: " + (  words.foldLeft(0) ((result: Int, element: String) => result + element.length) ))
+println("character size fold left: " + (  words.foldLeft(0) (_ + _.length)  ))
+println("character size fold right: " + (  words.foldRight(0) ((element: String, result: Int) => element.length + result)  ))
+println("character size fold right: " + (  words.foldRight(0) (_.length + _)  ))
+
+// fold left: your start value is at the left side. So that is the first type (startvalue type, listvalue type)
+// fold right: your start value is at the right side. So that is the second type (listvalue type, startvalue type) 
+
+// reverse a list using foldLeft
+
+def reverseLeft[T](xs: List[T]) = (List[T]() /: xs) {(ys, y) => y :: ys}
+println(reverseLeft(List(1, 2, 3, 4, 5)))
---
+++ ch16_list_object.scala
@@ -0,0 +1,13 @@
+// range
+
+println(List.range(1, 5))
+println(List.range(1, 9, 2))
+println(List.range(1, 10, 2))
+println(List.range(9, 1, -3))
+println(List.range(1, 9, -3))
+
+// fill
+
+println(List.fill(4)("hello"))
+println(List.fill(4)(2))
+println(List.fill(4, 3)(0))

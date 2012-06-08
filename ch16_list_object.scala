5c7f8202920d5d7459fa6a88fc129895dd4b5eff
Wiggert <wloonstra@ebay.com>
2012-06-08 08:31:49 +0200

    list object and working with multiple lists

--- ch16_list_object.scala
+++ ch16_list_object.scala
@@ -11,3 +11,9 @@ println(List.range(1, 9, -3))
 println(List.fill(4)("hello"))
 println(List.fill(4)(2))
 println(List.fill(4, 3)(0))
+
+// tabulate
+// like fill, but with function to define contents
+
+println(List.tabulate(10)(n => n * n))
+println(List.tabulate(5, 5)(_ * _))

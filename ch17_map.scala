1bffd3cfbd04ede607e9f7d0746ae817fda75061
Wiggert <wloonstra@ebay.com>
2012-06-13 08:52:56 +0200

    map functions

--- ch17_map.scala
+++ ch17_map.scala
@@ -22,3 +22,46 @@ def countWords(text: String) = {
 }
 
 println(countWords("See Spot run! Run, Spot. Run!"))
+
+// methods on Map
+
+val nums = Map("i" -> 1, "ii" -> 2)
+p(nums)
+p(nums + ("vi" -> 6))
+p(nums - "ii")
+p(nums ++ List("iii" -> 3, "v" -> 5))
+p(nums -- List("i", "ii"))
+p(nums.size)
+p(nums.contains("ii"))
+p(nums("ii"))
+p(nums.keys)
+p(nums.keySet)
+p(nums.values)
+p(nums.isEmpty)
+
+val words = mutable.Map.empty[String, Int]
+words += ("one" -> 1)
+p(words)
+
+words -= ("one")
+p(words)
+
+words ++= List("one" -> 1, "two" -> 2, "three" -> 3)
+p(words)
+
+words --= List("one", "two")
+p(words)
+
+// you can create mutable map same way, as long as you have correct import
+val bla = mutable.Map("bla" -> "foo")
+p(bla)
+
+// TREEMAP
+
+import scala.collection.immutable.TreeMap
+var tm = TreeMap(3 -> 'x', 1 -> 'x', 4 -> 'x') // remember: this is a var. So it is reassigned on +=
+p(tm)
+tm += (2 -> 'x')
+p(tm)
+
+def p(any: Any) = println(any.toString)

81726876552f8600a48b732e22e44514602241b3
Wiggert <wloonstra@ebay.com>
2012-06-21 08:27:14 +0200

    added Temperature example ch18

--- ch18_var.scala
+++ ch18_var.scala
@@ -17,3 +17,21 @@ time.hour = 13
 time.hour_=(13)
 
 println(time.hour)
+
+class Thermometer {
+  var celsius: Float = _
+  
+  def fahrenheit = celsius * 9 / 5 + 32
+  def fahrenheit_= (f: Float) {
+    celsius = (f - 32) * 5 / 9
+  }
+  override def toString = fahrenheit + "F/" + celsius + "C"
+}
+
+val t = new Thermometer
+t.celsius = 100
+println(t)
+t.fahrenheit = -40
+println(t)
+t.fahrenheit = 93
+println(t)
